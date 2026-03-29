102d9431c5ac6406f75968710a0b4ec630bbb423
Christianst_ <christianstun8@gmail.com>
2026-03-29 14:11:11 +0200

    Fixing bug (Forgotten tuples in the db_init have been fixed)

--- setup_config_db.py
+++ setup_config_db.py
@@ -14,8 +14,8 @@ async def config_setup_db(db: aiosqlite.Connection):
                                                              booster_id INTEGER,
                                                              member_id INTEGER
                         )""")
-    await db.execute("""INSERT OR IGNORE INTO roles (guild_id) VALUES (?)""", 1476359827176423426)
-    await db.execute("""INSERT OR IGNORE INTO roles(guild_id) VALUES (?)""", 1133464786252861633)
+    await db.execute("""INSERT OR IGNORE INTO roles (guild_id) VALUES (?)""", (1476359827176423426,))
+    await db.execute("""INSERT OR IGNORE INTO roles(guild_id) VALUES (?)""", (1133464786252861633,))
     await db.execute("""CREATE TABLE IF NOT EXISTS channels (
                                                                 guild_id INTEGER PRIMARY KEY,
                                                                 faq_channel_id INTEGER,
@@ -27,21 +27,21 @@ async def config_setup_db(db: aiosqlite.Connection):
                                                                 level_up_channel_id INTEGER,
                                                                 counting_channel_id INTEGER)
                      """)
-    await db.execute("""INSERT OR IGNORE INTO channels(guild_id ) VALUES (?)""", 1476359827176423426)
-    await db.execute("""INSERT OR IGNORE INTO channels(guild_id) VALUES (?)""", 1133464786252861633)
+    await db.execute("""INSERT OR IGNORE INTO channels(guild_id ) VALUES (?)""", (1476359827176423426,))
+    await db.execute("""INSERT OR IGNORE INTO channels(guild_id) VALUES (?)""", (1133464786252861633,))
     await db.execute("""CREATE TABLE IF NOT EXISTS categories (
                                                                 guild_id INTEGER PRIMARY KEY,
                                                                 open_tickets_cat_id INTEGER,
                                                                 closed_tickets_cat_id INTEGER,
                                                                 claimed_tickets_cat_id INTEGER)""")
-    await db.execute("""INSERT OR IGNORE INTO categories(guild_id) VALUES (?)""", 1476359827176423426)
-    await db.execute("""INSERT OR IGNORE INTO categories(guild_id) VALUES (?)""", 1133464786252861633)
+    await db.execute("""INSERT OR IGNORE INTO categories(guild_id) VALUES (?)""", (1476359827176423426,))
+    await db.execute("""INSERT OR IGNORE INTO categories(guild_id) VALUES (?)""", (1133464786252861633,))
     await db.execute("""CREATE TABLE IF NOT EXISTS spam(
                                                             guild_id INTEGER PRIMARY KEY,
                                                             messages_in_a_row INTEGER,
                                                             timewindow INTEGER)""")
-    await db.execute("""INSERT OR IGNORE INTO spam(guild_id) VALUES (?)""", 1476359827176423426)
-    await db.execute("""INSERT OR IGNORE INTO spam(guild_id) VALUES (?)""", 1133464786252861633)
+    await db.execute("""INSERT OR IGNORE INTO spam(guild_id) VALUES (?)""", (1476359827176423426,))
+    await db.execute("""INSERT OR IGNORE INTO spam(guild_id) VALUES (?)""", (1133464786252861633,))
     await db.commit()
 
 async def get_role_config(db: aiosqlite.Connection, guild_id: int):
